c79cbb3ae271d93882f2530e59630be9aa58d6cf
Teut2711 <manglavishesh64@gmail.com>
2026-02-12 00:37:38 +0530

    fix

---
+++ api/app/schemas/__init__.py
@@ -0,0 +1,11 @@
+from .product import ProductCreate, ProductUpdate, ProductOut
+from .category import CategoryCreate, CategoryUpdate, CategoryOut
+
+__all__ = [
+    "ProductCreate", 
+    "ProductUpdate",
+    "ProductOut",
+    "CategoryCreate",
+    "CategoryUpdate", 
+    "CategoryOut"
+]
