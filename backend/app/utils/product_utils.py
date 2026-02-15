a750a477c83ee5afefd302ce9e1296bee7018637
Teut2711 <manglavishesh64@gmail.com>
2026-02-15 19:24:04 +0530

    fix

--- backend/app/utils/product_utils.py
+++ backend/app/utils/product_utils.py
@@ -37,7 +37,6 @@ def map_product_to_read(product: Product) -> ProductRead:
         minimum_order_quantity=product.minimum_order_quantity,
         thumbnail=product.thumbnail,
         dimensions=ProductDimensionsRead(
-            id=product.dimensions.id,
             width=product.dimensions.width,
             height=product.dimensions.height,
             depth=product.dimensions.depth,
